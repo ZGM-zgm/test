589e839212e26f3101461c50499489290c7dc989
ZGM <2396657348@qq.com>
2020-09-03 10:16:56 +0800

    第二次提交

---
+++ src/com/gm/dao/impl/User.java
@@ -0,0 +1,10 @@
+package com.gm.dao.impl;
+
+import com.gm.dao.UserDao;
+
+public class User implements UserDao {
+    @Override
+    public void show() {
+        System.out.println("hello，world!");
+    }
+}
